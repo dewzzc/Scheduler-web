f24a764cec0705e6e303dc320089f79d656a1cc7
dewzzc <dewmina2007@gmail.com>
2026-06-18 19:37:15 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-const CACHE = 'workspace-schedule-v1';
-const FILES = ['./index.html', './manifest.json', './icon.svg'];
-
+const CACHE = 'workspace-schedule-v3';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './'
+];
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
